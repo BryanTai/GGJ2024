26ad7df66f1b56bfbbd464cb80a631d13d854b83
Michael Wright <beamer1989@gmail.com>
2024-01-27 22:47:36 -0800

    face changing

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -11,6 +11,10 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
 
+    public Sprite flyingFace;
+    public Sprite groundedFace;
+    public SpriteRenderer faceRenderer;
+
     private void Awake()
     {
         if(_rigidBody == null)
@@ -33,6 +37,14 @@ public class PlayerControls : MonoBehaviour
         if(_groundTrigger.IsGrounded && jumpPressed)
         {
             _rigidBody.AddForce(new Vector2(0f, _jumpForce));
+        }
+        if(_groundTrigger.IsGrounded)
+        {
+            faceRenderer.sprite = groundedFace;
+        }
+        else
+        {
+            faceRenderer.sprite = flyingFace;
         }
     }
 }
