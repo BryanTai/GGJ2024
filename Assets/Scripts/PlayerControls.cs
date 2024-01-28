8656758bf03f40101eee958979e0e86b457ce153
Bryan Tai <bryan.tai@alumni.ubc.ca>
2024-01-27 18:18:54 -0800

    Only jump if the player standing on a platform

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidBody;
-    [SerializeField] private float _speed = 3.0f;
-    [SerializeField] private float _jumpForce = 400f;
+    [SerializeField] private Collider2D _collider;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
 
+    private bool _isGrounded = false;
+
+    private int _groundMask;
 
     private void Awake()
     {
+        if(_collider == null)
+        {
+            _collider = GetComponent<Collider2D>();
+        }
+
         if(_rigidBody == null)
         {
             _rigidBody = GetComponent<Rigidbody2D>();
         }
+
+        _groundMask = LayerMask.GetMask("Platform");
     }
 
     // Update is called once per frame
@@ -26,9 +38,28 @@ public class PlayerControls : MonoBehaviour
 
         _rigidBody.velocity = new Vector2 (moveHorizontal * _speed, _rigidBody.velocity.y);
         
-        if(jumpPressed)
+        if(_isGrounded && jumpPressed)
         {
             _rigidBody.AddForce(new Vector2(0f, _jumpForce));
+            _isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        int layerMask = 1 << col.gameObject.layer; 
+        if(layerMask == _groundMask)
+        {
+            _isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        int layerMask = 1 << other.gameObject.layer;
+        if (layerMask == _groundMask)
+        {
+            _isGrounded = false;
         }
     }
 }
