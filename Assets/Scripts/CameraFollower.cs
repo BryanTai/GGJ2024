029eebb036490eab68add1125c78dc3138346222
Daryl Miller <hunter.of.dune@gmail.com>
2024-01-27 17:32:34 -0800

    Fixed camera following in DuneScene

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -6,17 +6,22 @@ public class CameraFollower : MonoBehaviour
 {
     public float YOffset = 6;
     private Camera _camera;
-    private Transform _playerTransform;
+    public Transform _playerTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         _camera = Camera.main;
+
+        if(_playerTransform == null)
+        {
+            GameObject.FindGameObjectWithTag("Player");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        _camera.transform.position = _playerTransform.position + (Vector3.up * YOffset);
+        _camera.transform.position = _playerTransform.position + (Vector3.up * YOffset)+ Vector3.back;
     }
 }
